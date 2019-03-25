163a938096532682c20b4d82c9164d1ada84040a
Justin Kerntz and Mathew Akre <justin@>
2019-03-25 11:52:19 -0700

    add gallon, quart, pint, cup with mathmatic equation to convert to liter with results

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+var gallonToLiter = function(gallons) {
+  return gallons/.26417;
+};
+var gallons = parseFloat(prompt("Enter # of gallons"));
+var result = gallonToLiter(gallons);
+alert(result);
+
+var quartsToLiter = function(quarts) {
+  return quarts/1.0567;
+};
+var quarts = parseFloat(prompt("Enter # of quarts"));
+var result = quartsToLiter(quarts);
+alert(result);
+
+var pintsToLiter = function(pints) {
+  return pints/2.1134;
+}
+var pints = parseFloat(prompt("Enter # of pints"));
+var result = pintsToLiter(pints);
+alert(result);
+
+var cupsToLiter = function(cups) {
+  return cups/4.2268
+}
+var cups = parseFloat(prompt("Enter # of cups"));
+var result = cupsToLiter(cups);
+alert(result);

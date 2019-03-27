033e8df63b8a1025e1f15441b84d035a2f027580
Justin Kerntz and Mustafa Ghasemi <justin@>
2019-03-27 16:45:01 -0700

    add jquery form selectors

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,30 @@
 var gallonToLiter = function(gallons) {
   return gallons/.26417;
 };
-var gallons = parseFloat(prompt("Enter # of gallons"));
+var gallons = $("#gallonsToLiter").val();
 var result = gallonToLiter(gallons);
 alert(result);
 
 var quartsToLiter = function(quarts) {
   return quarts/1.0567;
 };
-var quarts = parseFloat(prompt("Enter # of quarts"));
+var quarts = $("#quartsToLiter").val();
 var result = quartsToLiter(quarts);
 alert(result);
 
 var pintsToLiter = function(pints) {
   return pints/2.1134;
 }
-var pints = parseFloat(prompt("Enter # of pints"));
+var pints = $("#pintsToLiter").val();
 var result = pintsToLiter(pints);
 alert(result);
 
 var cupsToLiter = function(cups) {
   return cups/4.2268
 }
-var cups = parseFloat(prompt("Enter # of cups"));
+var cups = $("#cupsToLiter").val();
 var result = cupsToLiter(cups);
 alert(result);
 
-var inpurfromuser = prompt("Enter either gallons, quarts, pints, cups");
 
 // inpurfromuser + "ToLiter" = xToLiter(x)
